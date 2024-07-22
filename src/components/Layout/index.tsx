5161cd40c9959303853aaea16233fe51bbf2287d
Lư Chí Tâm <chitam.lu97@gmail.com>
2024-07-22 17:31:20 +0700

    adjust UI

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,88 +1,93 @@
-import { Box } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { LeftMenuDrawer } from "../Drawer/LeftMenuDrawer";
-import Navbar from "../Navigation/Navbar";
-import { useAccount } from "wagmi";
-import { chainsConfig } from "../../constants";
-import { SwitchChainModal } from "../Modal/SwitchChainModal";
+import { Box } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { LeftMenuDrawer } from '../Drawer/LeftMenuDrawer'
+import Navbar from '../Navigation/Navbar'
+import { useAccount } from 'wagmi'
+import { chainsConfig } from '../../constants'
+import { SwitchChainModal } from '../Modal/SwitchChainModal'
 
 interface Props {
-  children?: React.ReactNode;
-  title: string;
+  children?: React.ReactNode
+  title: string
 }
 
 const Layout: React.FC<Props> = ({ children, title }) => {
-  const [mounted, setMounted] = useState(false);
-  const drawerWidth = 313;
-  const [toogle, setToogle] = useState<number>(Date.now());
+  const [mounted, setMounted] = useState(false)
+  const [toogle, setToogle] = useState<number>(Date.now())
 
-  const { isConnected, chainId } = useAccount();
+  const { isConnected, chainId } = useAccount()
   const needSwitchChain =
-    isConnected && chainId && !chainsConfig.supportedChains.includes(chainId);
-  const [switchModalOpen, setSwitchModalOpen] = React.useState(false);
+    isConnected && chainId && !chainsConfig.supportedChains.includes(chainId)
+  const [switchModalOpen, setSwitchModalOpen] = React.useState(false)
 
   useEffect(() => {
-    setMounted(true);
-  }, []);
+    setMounted(true)
+  }, [])
 
   useEffect(() => {
     if (needSwitchChain) {
-      setSwitchModalOpen(true);
+      setSwitchModalOpen(true)
     } else {
-      setSwitchModalOpen(false);
+      setSwitchModalOpen(false)
     }
-  }, [needSwitchChain]);
+  }, [needSwitchChain])
 
-  if (!mounted) return null;
+  if (!mounted) return null
 
   const onToogle = () => {
-    setToogle(Date.now());
-  };
+    setToogle(Date.now())
+  }
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <Navbar title={title} drawerWidth={drawerWidth} onToogle={onToogle} />
-      <LeftMenuDrawer drawerWidth={drawerWidth} toogle={toogle} />
+    <Box>
+      <Navbar
+        title={title}
+        onToogle={onToogle}
+      />
+      {/* <LeftMenuDrawer drawerWidth={drawerWidth} toogle={toogle} /> */}
       <Box
-        component="main"
+        component='main'
         sx={{
           flexGrow: 1,
           p: 3,
-          height: "100vh",
-          overflow: "auto",
-          position: "relative",
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          height: '100vh',
+          overflow: 'auto',
+          position: 'relative',
+          width: { sm: `100%` },
         }}
       >
-        <Box sx={{ height: "130px" }}></Box>
         <Box
           sx={{
-            position: "fixed",
-            width: "400px",
-            height: "400px",
-            left: "calc(50% - 250px / 2 - 0.5px)",
-            top: "calc(50% - 380px / 2 + 1px)",
-            background: "#77ED91",
-            opacity: "0.5",
-            filter: "blur(202.95px)",
+            position: 'fixed',
+            width: '400px',
+            height: '400px',
+            left: 'calc(50% - 250px / 2 - 0.5px)',
+            top: 'calc(50% - 380px / 2 + 1px)',
+            background: '#77ED91',
+            opacity: '0.5',
+            filter: 'blur(202.95px)',
+            zIndex: 1,
           }}
         ></Box>
         <Box
           sx={{
-            justifyContent: "center",
-            alignItems: "center",
-            display: "flex",
-            flexDirection: "column",
+            justifyContent: 'center',
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column',
           }}
         >
           {children}
         </Box>
       </Box>
-      <SwitchChainModal open={switchModalOpen} onClose={() => {}}>
+      <SwitchChainModal
+        open={switchModalOpen}
+        onClose={() => {}}
+      >
         <></>
       </SwitchChainModal>
     </Box>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
