e4cc3e54495c0aba18939b68c1d747b6a119e810
Peter0x2 <peter0x2@outlook.com>
2024-12-30 22:00:35 +0800

    update keyring URL

--- src/constants/keyringConfig.ts
+++ src/constants/keyringConfig.ts
@@ -1,7 +1,7 @@
 import { ChainId } from '../types'
 
 export const keyringConfig: { [chainId: number]: string } = {
-  [ChainId.MAINNET]: "https://app.keyring.network/onboarding?policyId=3&ref=singularity",
-  [ChainId.SEPOLIA]: "https://uat-app.keyring.network/onboarding?policyId=4&ref=singularity",
-  [ChainId.HARDHAT]: "https://app.keyring.network/onboarding?policyId=3&ref=singularity",
+  [ChainId.MAINNET]: "https://app.keyring.network/policies/singularity",
+  [ChainId.SEPOLIA]: "https://uat-app.keyring.network/policies/singularity",
+  [ChainId.HARDHAT]: "https://app.keyring.network/policies/singularity",
 }
